61b78a992bf7a21292299e6915ad9e6f0703488d
softwarephil <se.phildimarco@gmail.com>
2019-06-02 16:09:07 -0500

    changes/fixes to the survey

--- webcase/src/app/surveydata.ts
+++ webcase/src/app/surveydata.ts
@@ -3,292 +3,289 @@ export class SurveyData{
 	get(): Object{
 		
 return {
-			"pages": [
-			 {
-			  "name": "Info",
-			  "elements": [
-			   {
-				"type": "text",
-				"name": "town",
-				"title": "What town do you live in?",
-				"isRequired": true,
-			   },
-			   {
-				"type": "dropdown",
-				"name": "grade",
-				"title": "What grade are you in?",
-				"isRequired": true,
-				"choices": [
-				 {
-				  "value": "1",
-				  "text": "1st"
-				 },
-				 {
-				  "value": "2",
-				  "text": "2nd"
-				 },
-				 {
-				  "value": "3",
-				  "text": "3rd"
-				 },
-				 {
-				  "value": "4",
-				  "text": "4th"
-				 },
-				 {
-				  "value": "5",
-				  "text": "5th"
-				 },
-				 {
-				  "value": "6",
-				  "text": "6th"
-				 },
-				 {
-				  "value": "7",
-				  "text": "7th"
-				 },
-				 {
-				  "value": "8",
-				  "text": "8th"
-				 },
-				 {
-				  "value": "9",
-				  "text": "Freshmen"
-				 },
-				 {
-				  "value": "10",
-				  "text": "Sophmore"
-				 },
-				 {
-				  "value": "11",
-				  "text": "Junior"
-				 },
-				 {
-				  "value": "12",
-				  "text": "Senior"
-				 }
-				]
-			   }
-			  ],
-			  "title": "Hello, tell us a bit about yourself"
-			 },
-			 {
-			  "name": "conflict1",
-			  "elements": [
-			   {
-				"type": "matrix",
-				"name": "conflict",
-				"title": "These questions look at how well you deal with conflict",
-				"isRequired": true,
-				"columns": [
-				 {
-				  "value": "1",
-				  "text": "Never"
-				 },
-				 {
-				  "value": "2",
-				  "text": "Almost never"
-				 },
-				 {
-				  "value": "3",
-				  "text": "Sometimes"
-				 },
-				 {
-				  "value": "4",
-				  "text": "Almost every time"
-				 },
-				 {
-				  "value": "5",
-				  "text": "Every time"
-				 }
-				],
-				"rows": [
-				 {
-				  "value": "1",
-				  "text": "Do you always have to have the last word in a disagreement?"
-				 },
-				 {
-				  "value": "2",
-				  "text": "It is hard to see another's point of view?"
-				 },
-				 {
-				  "value": "3",
-				  "text": "Do you always keep your opinion to yourself?"
-				 },
-				 {
-				  "value": "4",
-				  "text": "Have you been threatened for sharing your opinion?"
-				 },
-				 {
-				  "value": "5",
-				  "text": "Are you uncomfortable around conflict?"
-				 }
-				],
-				"isAllRowRequired": true
-			   }
-			  ],
-			  "title": "Conflict"
-			 },
-			 {
-			  "name": "anger1",
-			  "elements": [
-			   {
-				"type": "matrix",
-				"name": "anger",
-				"title": "These questions deal with anger",
-				"isRequired": true,
-				"columns": [
-				 {
-				  "value": "1",
-				  "text": "Never"
-				 },
-				 {
-				  "value": "2",
-				  "text": "Almost never"
-				 },
-				 {
-				  "value": "3",
-				  "text": "Sometimes"
-				 },
-				 {
-				  "value": "4",
-				  "text": "Almost every time"
-				 },
-				 {
-				  "value": "5",
-				  "text": "Every time "
-				 }
-				],
-				"rows": [
-				 {
-				  "value": "1",
-				  "text": "When you get angry do you think of hurting yourself?"
-				 },
-				 {
-				  "value": "2",
-				  "text": "Do you have the urge to scream to release your emotions?"
-				 },
-				 {
-				  "value": "3",
-				  "text": "When things are not going your way, do you have the urge to screen and hit something?"
-				 },
-				 {
-				  "value": "4",
-				  "text": "When things are not going as planned, do you want to hit others?"
-				 },
-				 {
-				  "value": "5",
-				  "text": "Do you have thoughts about hurting yourself or others?"
-				 },
-				 {
-				  "value": "6",
-				  "text": "When you get angry do you feel like punching the wall?"
-				 },
-				 {
-				  "value": "7",
-				  "text": "Do you keep your anger bottled up inside?"
-				 },
-				 {
-				  "value": "8",
-				  "text": "Do you have to scream or hit something to get your anger out?"
-				 },
-				 {
-				  "value": "9",
-				  "text": "Do you take your anger out on others?"
-				 },
-				 {
-				  "value": "10",
-				  "text": "How often do you get angry?"
-				 },
-				 {
-				  "value": "11",
-				  "text": "Does anyone around you take their anger out on you or the people you love?"
-				 }
-				],
-				"isAllRowRequired": true
-			   }
-			  ],
-			  "title": "Anger"
-			 },
-			 {
-			  "name": "selfesteem3",
-			  "elements": [
-			   {
-				"type": "matrix",
-				"name": "selfesteem",
-				"title": "These questions deal with your self-esteem",
-				"isRequired": true,
-				"columns": [
-				 {
-				  "value": "1",
-				  "text": "Never"
-				 },
-				 {
-				  "value": "2",
-				  "text": "Almost never"
-				 },
-				 {
-				  "value": "3",
-				  "text": "Sometimes"
-				 },
-				 {
-				  "value": "4",
-				  "text": "Almost every time"
-				 },
-				 {
-				  "value": "5",
-				  "text": "Every time "
-				 }
-				],
-				"rows": [
-				 {
-				  "value": "1",
-				  "text": "Do you think you are inferior to to others?"
-				 },
-				 {
-				  "value": "2",
-				  "text": "Have you ever been threatened by anyone before?"
-				 },
-				 {
-				  "value": "3",
-				  "text": "Have you threatened anyone before?"
-				 },
-				 {
-				  "value": "4",
-				  "text": "Do you feel like you are in danger for expressing your opinion?"
-				 },
-				 {
-				  "value": "5",
-				  "text": "Do you like what you see when you look in the mirror?"
-				 },
-				 {
-				  "value": "6",
-				  "text": "Do you remain friends with people that call you names or treat you badly?"
-				 },
-				 {
-				  "value": "7",
-				  "text": "Do you always focus on your weaknesses?"
-				 },
-				 {
-				  "value": "8",
-				  "text": "Do you compare yourself to friends and people around you?"
-				 },
-				 {
-				  "value": "9",
-				  "text": "Do you like you are inferior to others?"
-				 },
-				 {
-				  "value": "10",
-				  "text": "Do people you love the most talk negative to you or put you down?"
-				 }
-				],
-				"isAllRowRequired": true
-			   }
-			  ],
-			  "title": "Self-Esteem"
-			 }
-			]
-		   }
+	"pages": [
+	 {
+	  "name": "Info",
+	  "elements": [
+	   {
+		"type": "html",
+		"name": "about",
+		"html": "About this survey:\n<br>\nIn response to all of the school shootings and stabbings, also spending many years supporting youth and youth organization, we have come to the realization that it is not just the guns and the knives that are the problem.  The real challenge is the state of our youth! Homicide and suicide plague our youth!  Just as we are encouraging young people to use innovation to solve social problems, we have launched the CASE App. CASE stands for Conﬂict Anger and, Self-Esteem.  This is a way for young people to self-identify if any of these are a challenge for them.  We are hoping through this app, to provide data to empower youth to make a change if this is an area of concern. Ways"
+	   },
+	   {
+		"type": "dropdown",
+		"name": "question1",
+		"title": "What state do you live in?",
+		"isRequired": true,
+		"choices": [
+		 "AK",
+		 "AL",
+		 "AR",
+		 "AZ",
+		 "CA",
+		 "CO",
+		 "CT",
+		 "DC",
+		 "DE",
+		 "FL",
+		 "GA",
+		 "GU",
+		 "HI",
+		 "IA",
+		 "ID",
+		 "IL",
+		 "IN",
+		 "KS",
+		 "KY",
+		 "LA",
+		 "MA",
+		 "MD",
+		 "ME",
+		 "MH",
+		 "MI",
+		 "MN",
+		 "MO",
+		 "MS",
+		 "MT",
+		 "NC",
+		 "ND",
+		 "NE",
+		 "NH",
+		 "NJ",
+		 "NM",
+		 "NV",
+		 "NY",
+		 "OH",
+		 "OK",
+		 "OR",
+		 "PA",
+		 "PR",
+		 "PW",
+		 "RI",
+		 "SC",
+		 "SD",
+		 "TN",
+		 "TX",
+		 "UT",
+		 "VA",
+		 "VI",
+		 "VT",
+		 "WA",
+		 "WI",
+		 "WV",
+		 "WY"
+		]
+	   },
+	   {
+		"type": "text",
+		"name": "City",
+		"title": "What city do you live in?",
+		"isRequired": true
+	   },
+	   {
+		"type": "dropdown",
+		"name": "grade",
+		"title": "What grade are you in?",
+		"isRequired": true,
+		"choices": [
+		 {
+		  "value": "8",
+		  "text": "8th"
+		 },
+		 {
+		  "value": "9",
+		  "text": "9th"
+		 },
+		 {
+		  "value": "10",
+		  "text": "10th"
+		 },
+		 {
+		  "value": "11",
+		  "text": "11th"
+		 },
+		 {
+		  "value": "12",
+		  "text": "12th"
+		 }
+		]
+	   }
+	  ],
+	  "title": "Hello, tell us a bit about yourself"
+	 },
+	 {
+	  "name": "conflict1",
+	  "elements": [
+	   {
+		"type": "matrix",
+		"name": "conflict",
+		"title": "These questions look at how well you deal with conflict",
+		"isRequired": true,
+		"columns": [
+		 {
+		  "value": "1",
+		  "text": "Never"
+		 },
+		 {
+		  "value": "2",
+		  "text": "Almost never"
+		 },
+		 {
+		  "value": "3",
+		  "text": "Sometimes"
+		 },
+		 {
+		  "value": "4",
+		  "text": "Almost every time"
+		 },
+		 {
+		  "value": "5",
+		  "text": "Every time"
+		 }
+		],
+		"rows": [
+		 {
+		  "value": "1",
+		  "text": "Do you always have to have the last word in a disagreement?"
+		 },
+		 {
+		  "value": "2",
+		  "text": "It is hard to see another's point of view?"
+		 },
+		 {
+		  "value": "3",
+		  "text": "Do you always keep your opinion to yourself?"
+		 },
+		 {
+		  "value": "4",
+		  "text": "Have you been threatened for sharing your opinion?"
+		 },
+		 {
+		  "value": "5",
+		  "text": "Are you uncomfortable around conflict?"
+		 }
+		],
+		"isAllRowRequired": true
+	   }
+	  ],
+	  "title": "Conflict"
+	 },
+	 {
+	  "name": "anger1",
+	  "elements": [
+	   {
+		"type": "matrix",
+		"name": "anger",
+		"title": "These questions deal with anger",
+		"isRequired": true,
+		"columns": [
+		 {
+		  "value": "1",
+		  "text": "Never"
+		 },
+		 {
+		  "value": "2",
+		  "text": "Almost never"
+		 },
+		 {
+		  "value": "3",
+		  "text": "Sometimes"
+		 },
+		 {
+		  "value": "4",
+		  "text": "Almost every time"
+		 },
+		 {
+		  "value": "5",
+		  "text": "Every time "
+		 }
+		],
+		"rows": [
+		 {
+		  "value": "1",
+		  "text": "When you get angry do you think of hurting yourself?"
+		 },
+		 {
+		  "value": "2",
+		  "text": "Do you keep your anger bottled up inside?"
+		 },
+		 {
+		  "value": "3",
+		  "text": "Do you have to scream or hit something to get your anger out?"
+		 },
+		 {
+		  "value": "4",
+		  "text": "How often do you get angry?"
+		 },
+		 {
+		  "value": "5",
+		  "text": "Does anyone around you take their anger out on you or the people you love?"
+		 }
+		],
+		"isAllRowRequired": true
+	   }
+	  ],
+	  "title": "Anger"
+	 },
+	 {
+	  "name": "selfesteem3",
+	  "elements": [
+	   {
+		"type": "matrix",
+		"name": "selfesteem",
+		"title": "These questions deal with your self-esteem",
+		"isRequired": true,
+		"columns": [
+		 {
+		  "value": "1",
+		  "text": "Never"
+		 },
+		 {
+		  "value": "2",
+		  "text": "Almost never"
+		 },
+		 {
+		  "value": "3",
+		  "text": "Sometimes"
+		 },
+		 {
+		  "value": "4",
+		  "text": "Almost every time"
+		 },
+		 {
+		  "value": "5",
+		  "text": "Every time "
+		 }
+		],
+		"rows": [
+		 {
+		  "value": "1",
+		  "text": "Do you like what you see when you look in the mirror?"
+		 },
+		 {
+		  "value": "2",
+		  "text": "Do you remain friends with people that call you names or treat you badly?"
+		 },
+		 {
+		  "value": "3",
+		  "text": "Do you compare yourself to friends and people around you?"
+		 },
+		 {
+		  "value": "4",
+		  "text": "Do people you love the most talk negative to you or put you down?"
+		 },
+		 {
+		  "value": "5",
+		  "text": "Do you always focus on your weaknesses?"
+		 }
+		],
+		"isAllRowRequired": true
+	   }
+	  ],
+	  "title": "Self-Esteem"
+	 }
+	]
+   }
 }
 
 testSurveyResults():object {
@@ -308,12 +305,7 @@ testSurveyResults():object {
 		  "3": "1",
 		  "4": "1",
 		  "5": "1",
-		  "6": "1",
-		  "7": "1",
-		  "8": "1",
-		  "9": "1",
-		  "10": "1",
-		  "11": "1"
+
 		},
 		"selfesteem": {
 		  "1": "3",
@@ -321,11 +313,6 @@ testSurveyResults():object {
 		  "3": "3",
 		  "4": "3",
 		  "5": "3",
-		  "6": "3",
-		  "7": "3",
-		  "8": "3",
-		  "9": "3",
-		  "10": "3"
 		}
 	  }
 }
