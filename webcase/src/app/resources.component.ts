316cf0adeb387164ce05416c690bd29557f0274a
Ubuntu <ubuntu@ip-172-31-31-97.us-east-2.compute.internal>
2019-08-08 00:17:10 +0000

    fixing typos, added resources to resource page

--- webcase/src/app/resources.component.ts
+++ webcase/src/app/resources.component.ts
@@ -4,7 +4,7 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'resources-page',
   //templateUrl: './app.component.html',
-  templateUrl: './template/resources.component.html'
+  templateUrl: './template/resources.component.html',
 })
 
 export class ResourcesComponent implements OnInit {
--- webcase/src/app/surveydata.ts
+++ webcase/src/app/surveydata.ts
@@ -10,7 +10,7 @@ return {
 	   {
 		"type": "html",
 		"name": "about",
-		"html": "About this survey:\n<br>\nIn response to all of the school shootings and stabbings, also spending many years supporting youth and youth organization, we have come to the realization that it is not just the guns and the knives that are the problem.  The real challenge is the state of our youth! Homicide and suicide plague our youth!  Just as we are encouraging young people to use innovation to solve social problems, we have launched the CASE App. CASE stands for Conﬂict Anger and, Self-Esteem.  This is a way for young people to self-identify if any of these are a challenge for them.  We are hoping through this app, to provide data to empower youth to make a change if this is an area of concern. Ways"
+		"html": "About this survey:\n<br>\nIn response to all of the school shootings and stabbings, also spending many years supporting youth and youth organization, we have come to the realization that it is not just the guns and the knives that are the problem.  The real challenge is the state of our youth! Homicide and suicide plague our youth!  Just as we are encouraging young people to use innovation to solve social problems, we have launched the CASE survey. CASE stands for Conﬂict Anger and, Self-Esteem.  This is a way for young people to self-identify if any of these are a challenge for them.  We are hoping through this app, to provide data to empower youth to make a change if this is an area of concern."
 	   },
 	   {
 		"type": "dropdown",
