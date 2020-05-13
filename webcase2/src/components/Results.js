58f5ee120393ac7384b7b903525006de0513c0a2
Singh, Gurjot (gs741q) <gs741q@att.com>
2020-05-13 08:46:03 -0500

    [fixes] fixes to questions and results

--- webcase2/src/components/Results.js
+++ webcase2/src/components/Results.js
@@ -10,13 +10,17 @@ export default class Results extends React.Component {
     seResults: undefined
   };
   getResult = (answers) => {
-    const sum = answers.reduce((a,b) => parseInt(a) + parseInt(b));
-    if(sum <= 11) {
-      return `You're good!`;
-    } else if (sum <= 21 && sum > 11) {
-      return `You might want to keep an eye on this topic. Find people who encourage you to try your best, but don't insist on perfection. Most of us do better when the pressure is off.`
+    if (answers.length > 0) {
+      const sum = answers.reduce((a,b) => parseInt(a) + parseInt(b));
+      if (sum <= 11) {
+        return `You're good!`;
+      } else if (sum <= 21 && sum > 11) {
+        return `You might want to keep an eye on this topic. Find people who encourage you to try your best, but don't insist on perfection. Most of us do better when the pressure is off.`
+      } else {
+        return `This seems to be an area of struggle for you. Click on the resources tab to get support and help.`;
+      }
     } else {
-      return `This seems to be an area of struggle for you. Click on the resources tab to get support and help.`;
+      return `There were no answers to this section`;
     }
   };
   render() {
